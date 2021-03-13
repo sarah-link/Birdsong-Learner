df9cd4ee40b5219c4e307a51114458bd358c33ed
Sarah Link <slink2647@gmail.com>
2021-03-12 18:57:31 -0600

    Removing unused router

--- birdsong-learner/src/App.js
+++ birdsong-learner/src/App.js
@@ -1,12 +1,12 @@
 import React, { Fragment, useState, useEffect} from 'react';
 import './App.css';
 import SongIdentifier from "./Song_Identifier/SongIdentifier";
-
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
+// import { HashRouter, Route, Link } from "react-router-dom";
+//
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+// } from "react-router-dom";
 
 class App extends React.Component {
     constructor(props) {
@@ -15,42 +15,16 @@ class App extends React.Component {
 
     render() {
         return (
-            <Router>
-                <div className="App">
-                    {/*<NavBar />*/}
+            // <HashRouter basename='/'>
+            // <Router>
+            //     <div className="App">
+            //         {/*<NavBar />*/}
 
                     <SongIdentifier />
-
-                    {/*<Switch>    /!* overlays and popups *!/*/}
-                    {/*    <Route path="/login">*/}
-                    {/*    </Route>*/}
-
-                    {/*    <Route path="/signup">*/}
-                    {/*    </Route>*/}
-                    {/*</Switch>*/}
-
-                    {/*<Switch>    /!* main content *!/*/}
-                    {/*    <Route path="/builder">*/}
-                    {/*        <EncounterBuilder/>*/}
-                    {/*    </Route>*/}
-
-                    {/*    <Route path="/manager">*/}
-                    {/*    </Route>*/}
-
-                    {/*    <Route path="/library">*/}
-                    {/*    </Route>*/}
-
-                    {/*    <Route path="/join">*/}
-
-                    {/*    </Route>*/}
-
-                    {/*    <Route exact path="/">*/}
-                    {/*        <Home />*/}
-                    {/*    </Route>*/}
-                    {/*</Switch>*/}
-
-                </div>
-            </Router>
+            //
+            //     </div>
+            // </Router>
+            // </HashRouter>
         );
     }
 }
